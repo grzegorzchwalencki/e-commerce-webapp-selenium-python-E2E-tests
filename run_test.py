0ea79076b0acef1a3a8cf99623cd70922a003d64
greg-buka <bukacieszuka1@gmail.com>
2023-11-08 18:52:49 +0100

    Changed - pytest as tests runner, collected tests result data

--- run_test.py
+++ run_test.py
@@ -1,12 +1,7 @@
 import unittest
-import HtmlTestRunner
-import os
 from tests.create_account_test import CreateAccountTest
 from tests.sign_in_test import SignInTest
 from tests.category_test import WomenCategoryTest
-
-# Get the directory path to output report file
-dir = os.getcwd()
 
 # Load tests to run
 create_account_tests = unittest.TestLoader().loadTestsFromTestCase(CreateAccountTest)
@@ -22,12 +17,3 @@ tests_to_run = [
 
 # Create Test Suite - join tests
 test_suite = unittest.TestSuite(tests_to_run)
-
-# Open the report file
-outfile = open(dir + "\HTMLTestReport.html", "w")
-
-# configure HTMLTestRunner options
-runner = HtmlTestRunner.HTMLTestRunner(stream=outfile, combine_reports=True, report_name="TestReport")
-
-# Run tests
-runner.run(test_suite)
